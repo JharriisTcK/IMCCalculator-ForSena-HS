6980593afd7c4cc6f1891a0c98bc0d5a8bd1c059
Harry Silva <jharriistck@outlook.com>
2024-08-17 13:03:30 -0500

    V0.0 - Primera carga de aplicacion, solo interfaz grafica y cambios entre activities

---
+++ app/src/main/java/com/example/imccalculatorforsena/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.imccalculatorforsena
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btHome=findViewById<ImageView>(R.id.imageHome)
+        btHome.setOnClickListener {
+//            val intento=Intent(this, MainActivity::class.java)
+//            startActivity(intento)
+        }
+
+        val btUser=findViewById<ImageView>(R.id.imageUser)
+        btUser.setOnClickListener {
+            val intento=Intent(this, UserActivity::class.java)
+            startActivity(intento)
+        }
+
+        val btIniciarSesion=findViewById<Button>(R.id.btIniciar)
+        btIniciarSesion.setOnClickListener {
+            val intento=Intent(this, LoginActivity::class.java)
+            startActivity(intento)
+        }
+
+        val btRegistrar=findViewById<Button>(R.id.btRegistrar)
+        btRegistrar.setOnClickListener {
+            val intento=Intent(this, RegistroActivity::class.java)
+            startActivity(intento)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/imccalculatorforsena/UserActivity.kt
@@ -0,0 +1,34 @@
+package com.example.imccalculatorforsena
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btHome=findViewById<ImageView>(R.id.imageHome)
+        btHome.setOnClickListener {
+            val intento=Intent(this, MainActivity::class.java)
+            startActivity(intento)
+        }
+
+        val btUser=findViewById<ImageView>(R.id.imageUser)
+        btUser.setOnClickListener {
+//            val intento= Intent(this, UserActivity::class.java)
+//            startActivity(intento)
+        }
+    }
+}
